ad81124c99d09c2e317ccc112f0690b1da508319
EthanMathis <ethan.mathis@outlook.com>
2021-03-03 17:40:39 -0600

    completed up through tilling the field; next step is to sow the field

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,12 @@
-console.log("Welcome to the main module")
+import { createPlan } from "./plan.js"
+import { createSoybean } from "./seeds/soybean.js";
+import { addPlant, usePlants } from "./field.js"
 
+const yearlyPlan = createPlan();
+console.log(yearlyPlan);
+
+const soybeanSeed = createSoybean();
+addPlant(soybeanSeed);
+
+const fieldState = usePlants();
+console.log(fieldState);
